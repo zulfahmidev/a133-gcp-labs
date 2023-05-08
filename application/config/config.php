ade9fd2ca2c157063fdfdbb18efd9d6577d1a6d9
zulfahmidev <developer.zulfahmi@gmail.com>
2023-05-08 16:27:10 +0700

    repair

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // TODO: Sesuaikan dengan base url front-end yang digunakan
-// $config['base_url'] = 'base_url_frontend_Anda';
+$config['base_url'] = 'http://34.101.173.252/';
 
 /*
 |--------------------------------------------------------------------------
